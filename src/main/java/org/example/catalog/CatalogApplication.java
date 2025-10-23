63f56c50016a7ec4f4cbe5eeb1e6817fa977c012
Kapischan Sriganthan <kapischan.sriganthan@outlook.com>
2025-10-23 12:21:38 +0200

    Test Commit AB#140

--- src/main/java/org/example/catalog/CatalogApplication.java
+++ src/main/java/org/example/catalog/CatalogApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CatalogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CatalogApplication.class, args);
+        System.out.println("Hello World!");
+        SpringApplication.run(CatalogApplication.class, args);
 	}
 
 }
