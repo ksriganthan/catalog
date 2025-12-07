160d6d3fd145bf175cce5a77499d8543188442ee
Micaela Wieland <micaela.wieland@students.fhnw.ch>
2025-12-07 19:46:05 +0100

    Spring Boot Actuator Dependency hinzugefügt/ Versionsprobleme bei Tests gefixt

--- src/test/java/org/example/catalog/controller/BookControllerIntegrationTest.java
+++ src/test/java/org/example/catalog/controller/BookControllerIntegrationTest.java
@@ -1,9 +1,13 @@
 package org.example.catalog.controller;
 
 import org.example.catalog.PostgresContainerTest;
+import org.example.catalog.data.Author;
 import org.example.catalog.data.Book;
+import org.example.catalog.repository.AuthorRepository;
+import org.example.catalog.repository.BookRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -27,9 +31,35 @@ public class BookControllerIntegrationTest extends PostgresContainerTest {
 
     private RestClient restClient;
 
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+
     @BeforeEach
     void setup() {
         restClient = RestClient.create("http://localhost:" + port);
+        initData();
+    }
+    private void initData() {
+        Author a3 = new Author("Sebastian", "Fitzek");
+        Book b3 = new Book("9783426281758", "Der Nachbar", "Frau leidet an Monophobie, Nachbar verfolgt sie");
+        Book b5 = new Book("9783426519486", "Mimik", "Frau leidet an Monophobie, Nachbar verfolgt sie");
+
+        authorRepository.save(a3);
+        bookRepository.save(b3);
+        bookRepository.save(b5);
+
+        b3.getAuthors().add(a3);
+        b5.getAuthors().add(a3);
+        a3.getBooks().add(b3);
+        a3.getBooks().add(b5);
+
+        authorRepository.save(a3);
+        bookRepository.save(b3);
+        bookRepository.save(b5);
     }
 
     @Test
