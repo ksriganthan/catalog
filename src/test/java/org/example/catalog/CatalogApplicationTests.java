f1334c142b3d7c790427fffb63ba77381af22a71
Kapischan Sriganthan <kapischan.sriganthan@outlook.com>
2025-12-11 11:22:38 +0100

    kleine Bugfixes

--- src/test/java/org/example/catalog/CatalogApplicationTests.java
+++ src/test/java/org/example/catalog/CatalogApplicationTests.java
@@ -2,11 +2,11 @@ package org.example.catalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class CatalogApplicationTests {
+@Testcontainers
+class CatalogApplicationTests extends PostgresContainerTest {
 
 	@Test
 	void contextLoads() {
--- src/test/java/org/example/catalog/controller/BookControllerIntegrationTest.java
+++ src/test/java/org/example/catalog/controller/BookControllerIntegrationTest.java
@@ -1,13 +1,9 @@
 package org.example.catalog.controller;
 
 import org.example.catalog.PostgresContainerTest;
-import org.example.catalog.data.Author;
 import org.example.catalog.data.Book;
-import org.example.catalog.repository.AuthorRepository;
-import org.example.catalog.repository.BookRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -31,35 +27,9 @@ public class BookControllerIntegrationTest extends PostgresContainerTest {
 
     private RestClient restClient;
 
-    @Autowired
-    private BookRepository bookRepository;
-
-    @Autowired
-    private AuthorRepository authorRepository;
-
-
     @BeforeEach
     void setup() {
         restClient = RestClient.create("http://localhost:" + port);
-        initData();
-    }
-    private void initData() {
-        Author a3 = new Author("Sebastian", "Fitzek");
-        Book b3 = new Book("9783426281758", "Der Nachbar", "Frau leidet an Monophobie, Nachbar verfolgt sie");
-        Book b5 = new Book("9783426519486", "Mimik", "Frau leidet an Monophobie, Nachbar verfolgt sie");
-
-        authorRepository.save(a3);
-        bookRepository.save(b3);
-        bookRepository.save(b5);
-
-        b3.getAuthors().add(a3);
-        b5.getAuthors().add(a3);
-        a3.getBooks().add(b3);
-        a3.getBooks().add(b5);
-
-        authorRepository.save(a3);
-        bookRepository.save(b3);
-        bookRepository.save(b5);
     }
 
     @Test
@@ -77,6 +47,7 @@ public class BookControllerIntegrationTest extends PostgresContainerTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(list).isNotEmpty();
         assertThat(list.stream().anyMatch(b -> b.getTitle().contains("Mimik"))).isTrue();
+        System.out.println(list.size());
     }
 
     @Test
@@ -89,7 +60,6 @@ public class BookControllerIntegrationTest extends PostgresContainerTest {
                         .build())
                 .retrieve()// Führt die Anfrage wirklich aus
                 .toEntity(Book[].class); // Die JSON-Antwort wird immer in ein Book-Array gepackt
-
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEmpty();
     }
